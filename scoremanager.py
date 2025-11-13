f1c686b8eda902cd7038ee873580cf6aeefc0414
LeeringFish <theleeringfish@proton.me>
2025-11-12 21:19:20 -0500

    add scoremanager.py

---
+++ scoremanager.py
@@ -0,0 +1,22 @@
+import os.path
+
+class ScoreManager():
+    FILENAME = "./highscore.txt"
+    
+    def __init__(self):
+        self.high_score = self.read_high_score()
+        self.current_score = 0
+
+    def add_point(self):
+        self.current_score += 1
+
+    def read_high_score(self):
+        if os.path.isfile(self.FILENAME):
+            with open(self.FILENAME) as file:
+                score = file.read().strip()
+                try:
+                    return int(score)
+                except ValueError:
+                    return 0
+        else:
+            return 0
